4809e6b273726a6ed35664880cdb6bd2d57fa496
okomtayiw <wiyatmoko@gmail.com>
2021-08-25 10:23:13 +0700

    make rest api with jwt and php

---
+++ api/list_data_user.php
@@ -0,0 +1,100 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: http://localhost/apivisindo/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// required to encode json web token
+include_once '../config/core.php';
+include_once '../libs/php-jwt-master/src/BeforeValidException.php';
+include_once '../libs/php-jwt-master/src/ExpiredException.php';
+include_once '../libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once '../libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
+ 
+ 
+// database connection will be here
+// files needed to connect to database
+include_once '../config/database.php';
+include_once '../objects/user.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// instantiate product object
+$user = new User($db);
+$headers = getallheaders();
+$jwt = $headers["Authorization"];
+$idUser = $headers["ID"];
+
+
+if($jwt){
+ 
+    try {
+        // decode jwt
+        $decoded = JWT::decode($jwt, $key, array('HS256'));
+ 
+        if($_SERVER['REQUEST_METHOD'] === "GET"){
+
+                $users = $user->getUser($idUser);
+        
+                if($users > 0){
+        
+                $users_arr = array();
+        
+                foreach ($users as $row){
+        
+                   $users_arr[] = array(
+                     "id" => $row['id'],
+                     "first_name" => $row["first_name"],
+                     "last_name" => $row['last_name'],
+                     "email" => $row["email"],
+                     "number" => $row["number_customers"],
+                     "address" => $row["address_customers"]
+                   );
+                }
+        
+                 http_response_code(200); // Ok
+                 echo json_encode(array(
+                   "status" => 1,
+                   "data" => $users_arr
+                 ));
+        
+                }else{
+                    http_response_code(404); // no data found
+                    echo json_encode(array(
+                    "status" => 0,
+                    "message" => "No Projects found"
+                    ));
+        
+                }
+            } 
+    }
+    catch (exception $e) {
+        
+        // set response code
+        http_response_code(401);
+    
+        // tell the user access denied  & show error message
+        echo json_encode(array(
+            "message" => "Access denied.",
+            "error" => $e->getMessage()
+        ));
+    }
+   
+} else {
+
+      // set response code
+      http_response_code(401);
+ 
+      // tell the user access denied
+      echo json_encode(array("message" => "Access denied."));
+
+}
+
+    
+
+?>
---
+++ config/core.php
@@ -0,0 +1,12 @@
+<?php
+// show error reporting
+error_reporting(E_ALL);
+ 
+// set your default time-zone
+date_default_timezone_set('Asia/Jakarta');
+ 
+// variables used for jwt
+$key = "251289";
+$issued_at = time();
+$expiration_time = $issued_at + (60 * 60); // valid for 1 hour
+$issuer = "http://localhost/visindo";
